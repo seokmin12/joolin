8a45c3aa66f7cce710f5a2efd6e4ffc1ea3dd136
이석민 <seokmin@iseogmin-ui-MacBookPro.local>
2020-10-30 00:29:44 +0900

    go_next_page 추가 수정

--- js/go_next_page.js
+++ js/go_next_page.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 		if($('#use_form_agree').prop('checked') == false || $('#privacy_agree').prop('checked') == false){
 	    	alert('필수 약관에 동의 하셔야 합니다.');
 		}else{
-			$(location).attr('href','register.html');
+			$(location).attr('href','../register.html');
 		}
 	});
 });
